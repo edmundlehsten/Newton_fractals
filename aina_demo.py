59f2a6109804f5443d91ac031f0e95c436a55d39
ainamaeve <45844074+ainamaeve@users.noreply.github.com>
2018-12-16 00:03:10 +0100

    demo
    This is a demo, where I have used pretend values and functions for N,a,b,c,d and zeroes inorder to see if what i have done works. I also used the pcolor function just to see if the matrix A does what it should. I'll upload my edit to the main file to this branch that I think will be able to fit in the overall code... If I am correct in knowing the inputs used for tank 4

---
+++ aina_demo.py
@@ -0,0 +1,20 @@
+from scipy import *
+from pylab import *
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+def zeroes(x,y): ##pretend zero functionto test A out, has to be replaced with correct one if correct
+    return (sin(x**2 +y**2)/(x**2 +y**2))
+def plot(N,a,b,c,d):
+    #function plot has N,a,b,c,d as parameters. N eventually deterines the shape of the matrices.
+    #a,b,c,d are manually given numbers as intervals
+    X,Y =np.meshgrid(linspace(a,b,N),linspace(c,d,N),indexing='ij') 
+    #this creates the matrices X and Y
+    X=X.transpose() #This transposes them
+    Y=Y.transpose()
+    v_zeroes=np.vectorize(zeroes) #this is the vectorized function of zeroes,task 3(?)
+    A=(v_zeroes(X, Y) )#this creates a matrix A
+    pcolor(X,Y,A) #probably not correct but wanted to see if A does what it should
+    return X, Y, plt.plot(X, Y , marker='.', color='k', linestyle='none')
+plot(50,1,5,2,6)
