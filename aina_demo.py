a150e457a039ec748b9be2cf6f1af3828532584c
ainamaeve <45844074+ainamaeve@users.noreply.github.com>
2018-12-16 13:23:57 +0100

    Update aina_demo.py

--- aina_demo.py
+++ aina_demo.py
@@ -3,18 +3,40 @@ from pylab import *
 import matplotlib.pyplot as plt 
 import numpy as np
 
+ def plot(self, N, a, b, c, d):
+        """
+        where N eventually determines the size of the matrix and a,b,c,d are the maually
+        given intervals (a,c) corresponds to the bottom left corner of the grid 
+        and (b,d) the top right corner of the grid
+        """
+        Y, X=np.meshgrid(linspace(a,b,N),linspace(c,d,N),indexing='ij')
+        v_zeroes=np.vectorize(zeroes)
+        A=(v_zeroes(X,Y))
+        pcolor(X,Y,A)
+        return plt.plot(X,Y,marker='.',colour='k',linstyle='none'),
 
 def zeroes(x,y): ##pretend zero functionto test A out, has to be replaced with correct one if correct
     return (sin(x**2 +y**2)/(x**2 +y**2))
 def plot(N,a,b,c,d):
-    #function plot has N,a,b,c,d as parameters. N eventually deterines the shape of the matrices.
-    #a,b,c,d are manually given numbers as intervals
-    X,Y =np.meshgrid(linspace(a,b,N),linspace(c,d,N),indexing='ij') 
-    #this creates the matrices X and Y
-    X=X.transpose() #This transposes them
-    Y=Y.transpose()
-    v_zeroes=np.vectorize(zeroes) #this is the vectorized function of zeroes,task 3(?)
-    A=(v_zeroes(X, Y) )#this creates a matrix A
-    pcolor(X,Y,A) #probably not correct but wanted to see if A does what it should
+    """
+        where N eventually determines the size of the matrix and a,b,c,d are the maually
+        given intervals (a,c) corresponds to the bottom left corner of the grid 
+        and (b,d) the top right corner of the grid
+    """
+    Y,X =np.meshgrid(linspace(a,b,N),linspace(c,d,N),indexing='ij') 
+    """
+    this creates the transposed matrices X and Y
+    """
+    v_zeroes=np.vectorize(zeroes) 
+    """
+    this is the vectorized function of zeroes,task 3(?)
+    """
+    A=(v_zeroes(X, Y) )
+    """
+    this creates a matrix A
+    """
+    pcolor(X,Y,A) 
+    """probably not correct but wanted to see if A does what it should
+    """
     return X, Y, plt.plot(X, Y , marker='.', color='k', linestyle='none')
 plot(50,1,5,2,6)
