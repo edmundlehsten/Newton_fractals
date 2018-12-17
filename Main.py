c698c703f570edbbe31b4eb95011d2ea67410e93
NadineSteinbach <45844062+NadineSteinbach@users.noreply.github.com>
2018-12-17 10:42:22 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,17 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 16 21:13:55 2018
+
+@author: Nadine
+"""
+from scipy import *
 import numpy as np
+
 class fractal2D:
-    def __init__(self, f, derivative = None):
-        """
-        f : function taking a tuple and returning a tuple
-        der : derivative of the function taking a tuple and returning a 2x2 matrix (Jacobian matrix)
-        """
-        self.function = f
-        self.zeros = np.array([])
-        if derivative==None:
-            self.derivativeInput=str(None)
-        else:
-            self.derivativeInput=derivative
-       
     def partialDerivatives(self,x,y):
         """
         function that calculates the Jacobian of a DIFFERENTIABLE function
@@ -24,17 +20,27 @@ class fractal2D:
         ======
         J - Jacobian (partial derivatives) of a function
         """
-        if not (isinstance(x,(int,float)) and isinstance(y,(int,float))):
+        #print(type(x))  #if there is an error message here, print x to check the type
+        if not (isinstance(x,(int,float,np.int64,np.float64,np.int32,np.float32)) and isinstance(y,(int,float,np.int64,np.float64,np.int32,np.float32))):
             raise TypeError('The two input variables must be integers or floats.')
             
-        if self.derivativeInput=="None":
-            f=self.function
-            J=zeros(2)
-            h=1e-10
-            J=1/h*array([f(x+h,y)-f(x,y),f(x,y+h)-f(x,y)]).T
-            return J
+        f=self.function
+        J=zeros(2)
+        h=1e-10
+        J=1/h*array([f(x+h,y)-f(x,y),f(x,y+h)-f(x,y)]).T
+        return J
+        
+    def __init__(self, f, derivative = None):
+        """
+        f : function taking a tuple and returning a tuple
+        der : derivative of the function taking a tuple and returning a 2x2 matrix (Jacobian matrix)
+        """
+        self.function = f
+        self.zeros = np.array([])
+        if derivative==None:
+            self.derivative=self.partialDerivatives
         else:
-            return self.derivativeInput(x,y)
+            self.derivative=derivative
     
     def newtonMethod(self,guess):
         """
@@ -48,55 +54,18 @@ class fractal2D:
         ======
         zero - loaction of zero, returns None if the guess did not converge (tuple)
         """
-        if isinstance(guess,(list,tuple)) and size(guess)==2:
+        if isinstance(guess,(list,tuple,np.ndarray)) and size(guess)==2:
             guess=array(guess)
-        elif not (str(type(guess))=="<class 'numpy.ndarray'>" and size(guess)==2):
+        else:
             raise TypeError('The initial guess is not of the correct type. It must be a list with two integer/ float entries, a tuple or an array of size (2,)')
         
         maxloop = 1000
         new_guess = array([0,0]) #initiate value here so that it dose not get reinitiated for each loop (to improve performance)
         for i in range(maxloop):
-            new_guess = guess - np.matmul(np.linalg.inv(self.partialDerivatives(guess[0],guess[1])),f(guess[0],guess[1]))#should work but need a R^2 function and derivative to test...
+            new_guess = guess - np.matmul(np.linalg.inv(self.derivative(guess[0],guess[1])),f(guess[0],guess[1]))#should work but need a R^2 function and derivative to test...
             if np.abs(new_guess[0]-guess[0]) < 10**(-5) and np.abs(new_guess[1]-guess[1]) < 10**(-5): #close enough to a zero value...
                 return new_guess
             guess = new_guess
         else:
             return None  # return None if did not converge
         
-
-# %% new cell
-        
-    def find_zero(self, guess):
-        """
-        function that carries out the newton integration method
-        Author: Edmund
-        Inputs
-        ======
-        guess - initial guess to for a zero
-        
-        Output
-        ======
-        returns index of zero, None if point did not converge
-        """
-        val = newtonMethod(guess)
-        if val is None: # value did not converge
-            return None        
-        if (self.zeros-val < 10**-5).any: #zero already exists
-            np.where(self.zeros-val < 10**-5)
-        else:  # value dose not exist yet
-            np.append(self.zeros,val) #add value to zeros
-            return self.zeros.size-1
-    
-    def plot(self):
-        """
-        (Task 4)
-        TODO: Write description of method... 
-        """
-        return
-    def simpleNewtonMethod(self):
-        """
-        (task 5)
-        TODO: write method descriptiion and method...
-        """
-        return
-    
